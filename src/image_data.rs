3b827b9528a4f778ece4c6edee194767b58c5d8d
Darian <dariandzirko@gmail.com>
2023-02-18 12:29:15 -0700

    Removed a lot of errors

--- src/image_data.rs
+++ src/image_data.rs
@@ -1,4 +1,11 @@
+use std::slice;
+
 use ndarray::Array2;
+
+use crate::{
+    check_error, format::Rs2Format, get_frame_info, rs2_error, rs2_frame, rs2_get_frame_data,
+    BITS_IN_A_BYTE,
+};
 
 pub struct BetterRawPixel {
     k: u8,
@@ -15,7 +22,7 @@ impl BetterRawPixel {
             r: 0,
             g: 0,
             b: 0,
-            a: u8::max,
+            a: u8::MAX,
         }
     }
 
@@ -27,7 +34,7 @@ impl BetterRawPixel {
             r: r,
             g: g,
             b: b,
-            a: u8::max,
+            a: u8::MAX,
         }
     }
 
@@ -38,20 +45,21 @@ impl BetterRawPixel {
 struct ImageData {
     raw_data: Array2<u8>, //this size should be height * stride, where stride is width*bytes per pixel
     pub format: Rs2Format,
-    pub width: u32,
-    pub height: u32,
-    pub bits_per_pixel: u32,
-    pub stride: u32,
+    pub width: usize,
+    pub height: usize,
+    pub bits_per_pixel: usize,
+    pub bytes_per_pixel: usize,
+    pub stride: usize,
 }
 
 impl ImageData {
     pub fn new(
         format: Rs2Format,
-        width: u32,
-        height: u32,
-        bits_per_pixel: u32,
-        bytes_per_pixel: u32,
-        stride: u32,
+        width: usize,
+        height: usize,
+        bits_per_pixel: usize,
+        bytes_per_pixel: usize,
+        stride: usize,
     ) -> ImageData {
         Self {
             raw_data: Array2::u8(height, stride),
@@ -59,44 +67,57 @@ impl ImageData {
             width: width,
             height: height,
             bits_per_pixel: bits_per_pixel,
-            bytes_per_pixel: bits_per_pixel / BITS_IN_A_BYTE,
+            bytes_per_pixel: bits_per_pixel / (BITS_IN_A_BYTE as usize),
             stride: stride,
         }
     }
 
-    pub unsafe fn copy_data_from_frame(frame: *mut rs2_frame) {
-        for row in 0..height {
-            for col in 0..stride {
-                raw_data[[row, col]] = slice.get_unchecked((row * stride + col * bytes_per_pixel));
+    pub unsafe fn copy_data_from_frame(&mut self, frame: *mut rs2_frame) {
+        let mut error = std::ptr::null_mut::<rs2_error>();
+
+        let frame_info = get_frame_info(frame);
+        check_error(error);
+
+        let frame_data = rs2_get_frame_data(frame, &mut error);
+        check_error(error);
+
+        let slice = slice::from_raw_parts(frame_data.cast::<u8>(), self.bits_per_pixel as usize);
+
+        for row in 0..self.height {
+            for col in 0..self.stride {
+                self.raw_data[[row, col]] =
+                    *slice.get_unchecked(row * self.stride + col * self.bytes_per_pixel);
             }
         }
     }
 
-    pub fn get_better_raw_pixel(row: usize, col: usize) -> BetterRawPixel {
-        match format {
+    pub fn get_better_raw_pixel(&self, row: usize, col: usize) -> BetterRawPixel {
+        match self.format {
             Rs2Format::RGB8 => {
-                return from_rgb(
-                    raw_data[[row, col]],
-                    raw_data[[row, col + 1]],
-                    raw_data[[row, col + 2]],
+                return BetterRawPixel::from_rgb(
+                    self.raw_data[[row, col]],
+                    self.raw_data[[row, col + 1]],
+                    self.raw_data[[row, col + 2]],
                 );
             }
 
             Rs2Format::Y16 => {
                 //I could make this just return the u16s but I don't really think it would be all that useful in regards to
                 //displayng the data
-                let temp = ((raw_data[[row, col]] << 8) | raw_data[[row, col + 1]]);
-                return from_k((temp / u16::max * u8::max));
+                let temp: u16 = ((self.raw_data[[row, col]] as u16) << BITS_IN_A_BYTE as u16)
+                    | self.raw_data[[row, col + 1]] as u16;
+                return BetterRawPixel::from_k(temp / u16::MAX * u8::MAX as u16);
             }
 
             Rs2Format::Z16 => {
                 //Depth data is seemingly also just in black and white
-                let temp = ((raw_data[[row, col]] << 8) | raw_data[[row, col + 1]]);
-                return from_k((temp / u16::max * u8::max));
+                let temp: u16 = ((self.raw_data[[row, col]] as u16) << 8)
+                    | self.raw_data[[row, col + 1]] as u16;
+                return BetterRawPixel::from_k((temp / u16::MAX * u8::MAX));
             }
 
             _ => {
-                println!("I have not worked on this case yet")
+                unimplemented!("I have not worked on this format yet");
             }
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 mod bindings;
 mod frame;
+mod image_data;
 mod types;
 mod utils;
 
 pub use bindings::*;
 pub use frame::*;
+pub use image_data::*;
 pub use types::*;
 pub use utils::*;
